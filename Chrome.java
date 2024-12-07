8a64c35b118e5cf2151fe5abb0275e6bcf594330
ashik1703 <ashik.1703@yahoo.com>
2024-12-07 20:28:34 +0530

    Add files via upload
    day1 assignment

---
+++ Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The browser version is "+browserVersion);
+		System.out.println("The developer is "+developer);
+		System.out.println("Is Beta value "+isBeta);
+		System.out.println("Year of release is "+releaseYear);
+		System.out.println("Shortcut key is "+shortcutKey);
+		
+		
+
+	}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("The browser version is "+browserVersion);
+		System.out.println("Name of the browser is "+browserName);
+		System.out.println("Is the browser visible "+isVisible);
+		System.out.println("The browser was released on "+releaseYear);
+		System.out.println("The browser logo is "+browserLogo);
+		
+		
+		
+	}
+
+}
